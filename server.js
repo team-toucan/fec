00a70b1e682c005e6e7df97adbe080222854ad8d
Sam <sgeoffrey006@gmail.com>
2021-10-28 19:40:25 -0400

    created server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+//middleware
+app.use(express.json());
+
+app.use(express.static('./dist'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+
+})
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if(err) {
+    console.log(err)
+  } else {
+  console.log(`sever running on port ${PORT}`)
+  }});
