95ec1290277637debe7fdb57556b94352a73d1b0
Richard Hyesung O <orichh@gmail.com>
2021-11-02 19:49:30 -0400

    Switch files from ImageGallery and ProductInfo as they were mistakenly mixed up

--- src/components/Overview/ImageGallery/index.js
+++ src/components/Overview/ImageGallery/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import DefaultView from '@components/Overview/ImageGallery/DefaultView.jsx';
 
-const Heading = () => {
-  return <h1>Image Gallery</h1>;
+const Heading = ({ state }) => {
+  return (
+    <div>
+      <h1>Image Gallery</h1>
+      <DefaultView state={state} />
+    </div>
+  );
 };
 
 export default Heading;
