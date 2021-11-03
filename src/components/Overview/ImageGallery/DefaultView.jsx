3a62e477058e27677495f92d04594f6a02d79524
Richard Hyesung O <orichh@gmail.com>
2021-11-03 19:02:17 -0400

    Render first image

--- src/components/Overview/ImageGallery/DefaultView.jsx
+++ src/components/Overview/ImageGallery/DefaultView.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Heading = () => {
-  return <h1>test</h1>;
+const DefaultView = ({ state }) => {
+  if (state.productStyleById.results !== undefined) {
+    console.log(
+      '🛶',
+      state.productStyleById.results[0].photos[0].thumbnail_url
+    );
+  }
+  return (
+    <div>
+      <h1>test</h1>
+      {state.productStyleById.results !== undefined && (
+        <img src={state.productStyleById.results[0].photos[0].thumbnail_url} />
+      )}
+    </div>
+  );
 };
 
-export default Heading;
+export default DefaultView;
