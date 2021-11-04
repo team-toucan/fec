58df5e7123bc692592b03a31b4c08dd8f6a1311f
Richard Hyesung O <orichh@gmail.com>
2021-11-04 14:36:11 -0400

    Add placeholders for social sharing

--- src/components/Overview/ProductInfo/Share.jsx
+++ src/components/Overview/ProductInfo/Share.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Share = ({ state }) => {
   return (
     <div>
-      <h1>Share</h1>
+      <h1>Instagram</h1>
+      <h1>Twitter</h1>
+      <h1>Pinterest</h1>
     </div>
   );
 };
