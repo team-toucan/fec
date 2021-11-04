4a5a1441cc31d3e6729c56c5d9fdebe37a3eeb88
Richard Hyesung O <orichh@gmail.com>
2021-11-04 13:34:08 -0400

    Import sub-components and add into ProductInfo component

--- src/components/Overview/ProductInfo/index.js
+++ src/components/Overview/ProductInfo/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Price from '@components/Overview/ProductInfo/Price.jsx';
+import ProductCategory from '@components/Overview/ProductInfo/ProductCategory.jsx';
+import ProductOverview from '@components/Overview/ProductInfo/ProductOverview.jsx';
+import ProductTitle from '@components/Overview/ProductInfo/ProductTitle.jsx';
+import Share from '@components/Overview/ProductInfo/Share.jsx';
+import StarRating from '@components/Overview/ProductInfo/StarRating.jsx';
 
-const Heading = () => {
-  return <h1>Product Info</h1>;
+const ProductInfo = ({ state }) => {
+  return (
+    <div>
+      <h1>Product Info</h1>
+      <Price state={state} />
+      <ProductCategory state={state} />
+      <ProductOverview state={state} />
+      <ProductTitle state={state} />
+      <Share state={state} />
+      <StarRating state={state} />
+    </div>
+  );
 };
 
-export default Heading;
+export default ProductInfo;
