60356292a5aa031f8669f812cb547bc53613081c
Richard Hyesung O <orichh@gmail.com>
2021-11-04 14:27:06 -0400

    Render product overview, slogan, description, and features

--- src/components/Overview/ProductInfo/ProductOverview.jsx
+++ src/components/Overview/ProductInfo/ProductOverview.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const ProductOverview = ({ state }) => {
   return (
     <div>
-      <h1>ProductOverview</h1>
+      <h1>Product overview</h1>
+      <h1>{state.productById.slogan}</h1>
+      <h1>{state.productById.description}</h1>
+      <div>
+        {state.productById.features !== undefined &&
+          state.productById.features.map((feature, idx) => {
+            return (
+              <>
+                <h2 key={idx}>{feature.feature}</h2>
+                <h2>{feature.value}</h2>
+              </>
+            );
+          })}
+      </div>
     </div>
   );
 };
