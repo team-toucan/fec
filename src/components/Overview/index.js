9ad405b442bbe3af684b644038db9e060c6a31dc
Michael Christenson <mchristenson5891@gmail.com>
2021-11-05 10:23:43 -0700

    add notes to Overview -> index.js

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -4,6 +4,9 @@ import ProductInfo from '@components/Overview/ProductInfo';
 import StyleSelector from '@components/Overview/StyleSelector';
 import AddToCart from '@components/Overview/AddToCart';
 
+// import { useParams } from 'react-router-dom'; <--- import useParams hook from react router dom
+// https://v5.reactrouter.com/web/api/Hooks/useparams
+
 import { getAllProducts, getProductById, getProductStyleById } from '../../api';
 
 const tempState = {
@@ -12,11 +15,14 @@ const tempState = {
   currentStyle: 0,
   currentSize: 1356838,
 };
+// Move this state ^^^ inside the Overview component and use hooks, useState https://www.robinwieruch.de/react-usestate-hook
 
 getProductById(39333).then((res) => {
   console.log('success', res.data);
   tempState.productById = res.data;
 });
+
+// ^^^ make this api inside of `useEffect` https://www.robinwieruch.de/react-useeffect-hook. You can get the id of the current product by using the useParams hook.
 
 getProductStyleById(39333).then((res) => {
   console.log('styles', res.data);
