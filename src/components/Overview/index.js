0a58670519eacb1901d0383612c5f42b80cae1f0
Richard Hyesung O <orichh@gmail.com>
2021-11-08 19:41:01 -0500

    Removed local test state and added state using hooks and async await. Pass down state update function

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import ImageGallery from '@components/Overview/ImageGallery';
 import ProductInfo from '@components/Overview/ProductInfo';
 import StyleSelector from '@components/Overview/StyleSelector';
 import AddToCart from '@components/Overview/AddToCart';
 import { OverviewDiv } from './overviewStyles';
-
-// import { useParams } from 'react-router-dom'; <--- import useParams hook from react router dom
-// https://v5.reactrouter.com/web/api/Hooks/useparams
-
 import { getAllProducts, getProductById, getProductStyleById } from '../../api';
 
-const tempState = {
-  productById: {},
-  productStyleById: {},
-  currentStyle: 0,
-  currentSize: 1356838,
-};
-// Move this state ^^^ inside the Overview component and use hooks, useState https://www.robinwieruch.de/react-usestate-hook
-
-getProductById(39333).then((res) => {
-  console.log('success', res.data);
-  tempState.productById = res.data;
-});
-
-// ^^^ make this api inside of `useEffect` https://www.robinwieruch.de/react-useeffect-hook. You can get the id of the current product by using the useParams hook.
-
-getProductStyleById(39333).then((res) => {
-  console.log('styles', res.data);
-  tempState.productStyleById = res.data;
-});
-
 const Overview = () => {
-  console.log('temp state', tempState);
+  const { id } = useParams();
+  const [state, updateState] = useState({
+    productById: {},
+    productStyleById: {},
+    currentStyle: 0,
+    currentSize: '',
+    currentPhoto: 0,
+  });
+
+  useEffect(async () => {
+    const { data } = await getProductById(id);
+    updateState((prevValues) => {
+      return { ...prevValues, productById: data };
+    });
+  }, []);
+
+  useEffect(async () => {
+    const { data } = await getProductStyleById(id);
+    updateState((prevValues) => {
+      return { ...prevValues, productStyleById: data };
+    });
+  }, []);
+
+  /* TODO: update currentStyle based on user selection */
+  /* TODO: update name of style */
+  /* update price of style */
+  /* update sizes available for style */
+  /* update quantities available for each size */
+  /* update thumbnail photos */
+  /* update main photo */
+
+  console.log('new state 🎃', state);
+
   return (
     <OverviewDiv>
-      <h1>Overview Component</h1>
-      <ImageGallery state={tempState} />
-      <ProductInfo state={tempState} />
-      <StyleSelector state={tempState} />
-      <AddToCart state={tempState} />
+      <ImageGallery state={state} updateState={updateState} />
+      <ProductInfo state={state} updateState={updateState} />
+      <StyleSelector state={state} updateState={updateState} />
+      <AddToCart state={state} updateState={updateState} />
     </OverviewDiv>
   );
 };
