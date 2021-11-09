02e5e3b1989194a11245007d2f22f011b8c68236
Richard Hyesung O <orichh@gmail.com>
2021-11-09 16:30:12 -0500

    Pass in updateState as prop and uncomment QuantitySelector

--- src/components/Overview/AddToCart/index.js
+++ src/components/Overview/AddToCart/index.js
@@ -3,13 +3,13 @@ import AddToCartSub from '@components/Overview/AddToCart/AddToCartSub.jsx';
 import QuantitySelector from '@components/Overview/AddToCart/QuantitySelector.jsx';
 import SizeSelector from '@components/Overview/AddToCart/SizeSelector.jsx';
 
-const AddToCart = ({ state }) => {
+const AddToCart = ({ state, updateState }) => {
   return (
     <div>
       <h1>Add to Cart</h1>
-      <SizeSelector state={state} />
-      {/* <QuantitySelector state={state} /> */}
-      <AddToCartSub state={state} />
+      <SizeSelector state={state} updateState={updateState} />
+      <QuantitySelector state={state} updateState={updateState} />
+      <AddToCartSub state={state} updateState={updateState} />
     </div>
   );
 };
