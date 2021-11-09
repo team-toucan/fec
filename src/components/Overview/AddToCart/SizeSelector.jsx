74ad6131ca61c06780cc2c776dae663f217b7b03
Richard Hyesung O <orichh@gmail.com>
2021-11-09 16:15:13 -0500

    Add event handler for when size is selected. State's currentSize updated to option selected

--- src/components/Overview/AddToCart/SizeSelector.jsx
+++ src/components/Overview/AddToCart/SizeSelector.jsx
@@ -4,17 +4,27 @@ import React from 'react';
 // that houses the current style/style to change the size
 // based on the user selection below
 
-const SizeSelector = ({ state }) => {
+const SizeSelector = ({ state, updateState }) => {
+  const sizeSelected = (e) => {
+    e.preventDefault();
+    console.log('selection', e.target.value);
+    updateState((prevValues) => {
+      return {
+        ...prevValues,
+        currentSize: e.target.value,
+      };
+    });
+  };
   return (
     <div>
-      <select>
+      <select onChange={sizeSelected}>
         <option>Select Size</option>
         {state.productStyleById.results !== undefined &&
           Object.keys(
             state.productStyleById.results[state.currentStyle].skus
           ).map((size, idx) => {
             return (
-              <option key={idx}>
+              <option key={idx} value={size}>
                 {
                   state.productStyleById.results[state.currentStyle].skus[size]
                     .size
